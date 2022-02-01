46e74d29fdb4479ac625d1a34d5feb6d6890ae77
sc-github-dev <wodaguge369@gmail.com>
2022-02-01 18:21:49 -0500

    Added projects page and link to it

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__, template_folder="src", static_folder="dist")
 def index():
     return render_template("index.html")
     
+@app.route("/projects")
+def projects():
+    return "hi"
+    
 if __name__ == '__main__':
     app.run(debug=True)
