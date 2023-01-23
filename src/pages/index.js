966afabc47805c73e41365a08e1de96a592c3939
Samuel Chow <wodaguge369@gmail.com>
2023-01-23 18:00:04 -0500

    rewrite to gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
